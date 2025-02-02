c08e7fd547ac374037bc16cf31d259a0c42d5185
Kudryavtsev Nikita <113279023+NikRader@users.noreply.github.com>
2025-02-02 11:21:14 +0300

    added clean_architecture in project

---
+++ data/src/main/java/com/example/data/api/Api.kt
@@ -0,0 +1,10 @@
+package com.example.data.api
+
+import android.content.Context
+
+class Api(val context: Context) {
+    fun get_data_from_api(): String {
+        val BaseUrl = "https://stepik.org/api/courses/"
+        return ""
+    }
+}
